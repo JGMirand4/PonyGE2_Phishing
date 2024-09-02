c3e4a1cb5f3cefb50be47ef5c1248d46d091edb1
João Guilherme Miranda <114231096+JGMirand4@users.noreply.github.com>
2024-09-02 10:57:51 -0300

    Create gs_fitness.py

---
+++ GS/gs_fitness.py
@@ -0,0 +1,5 @@
+def evaluate_fitness(phenotype):
+    # Aqui você define como a avaliação da fitness será feita
+    # Pode ser baseada em algum critério específico de seu problema
+    fitness = some_fitness_function(phenotype)
+    return fitness
