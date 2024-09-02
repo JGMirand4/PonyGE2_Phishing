74e9bc2db3a74a9e0677a1139dcde0a14b7df3c4
João Guilherme Miranda <114231096+JGMirand4@users.noreply.github.com>
2024-09-02 10:58:47 -0300

    Create cfg_gp.py

---
+++ CFG-GP/cfg_gp.py
@@ -0,0 +1,22 @@
+from deap import base, creator, tools, gp
+import random
+
+def protected_div(left, right):
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 1
+
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.addPrimitive(protected_div, 2)
+pset.addPrimitive(lambda x, y: x + y, 2)
+pset.addPrimitive(lambda x, y: x - y, 2)
+pset.addTerminal(1)
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
