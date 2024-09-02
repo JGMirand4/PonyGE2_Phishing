ad78569834eefc14b7fb4425dad99a6c86011731
João Guilherme Miranda <114231096+JGMirand4@users.noreply.github.com>
2024-09-02 10:55:55 -0300

    Update sge_crossover.py

--- SGE/sge_crossover.py
+++ SGE/sge_crossover.py
@@ -1,1 +1,12 @@
+import numpy as np
+
+def sge_crossover(parent1, parent2):
+    child1, child2 = {}, {}
+    for non_terminal in parent1:
+        crossover_point = np.random.randint(1, len(parent1[non_terminal]))
+        child1[non_terminal] = np.concatenate((parent1[non_terminal][:crossover_point],
+                                               parent2[non_terminal][crossover_point:]))
+        child2[non_terminal] = np.concatenate((parent2[non_terminal][:crossover_point],
+                                               parent1[non_terminal][crossover_point:]))
+    return child1, child2
 
